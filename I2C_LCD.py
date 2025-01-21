f943ff19ceaf4542b5c1d18f0a23591dacd6bf03
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2025-01-21 09:46:25 +0900

    Update I2C_LCD.py

--- I2C_LCD.py
+++ I2C_LCD.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
+import LCD1602
+import time
 
-import I2C_LCD_driver
-from time import *
+def setup():
+    LCD1602.init(0x3f, 1)
+    LCD1602.write(0, 0, 'Hello World!!')
+    LCD1602.write(5, 1, '- RPi 400 -')
+    time.sleep(2)
 
-mylcd = I2C_LCD_driver.lcd()
-mylcd.lcd_display_string("Hello World",1)
-mylcd.lcd_display_string("Raspberry Pi3 b+",2)
+def destroy():
+    pass
+
+if __name__ == "__main__":
+    try:
+        setup()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        destroy()
