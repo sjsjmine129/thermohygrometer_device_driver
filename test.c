ab27522464e45af9df885ae8d3594c9c4dc08353
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2025-01-21 11:21:01 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,9 @@
 https://github.com/DavidAntliff/esp32-i2c-lcd1602.git
+
+
+$ git clone --recursive https://github.com/DavidAntliff/esp32-i2c-lcd1602-example.git
+
+$ cd esp32-i2c-lcd1602-example.git
+$ idf.py menuconfig    # set your serial configuration and the I2C GPIO - see below
+$ idf.py build
+$ idf.py -p (PORT) flash monitor
