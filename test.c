9e207c08cb59929760ea93568a03de2a93e9dea2
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2025-01-21 10:07:48 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,33 +1,98 @@
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
- 
-int main() {
-    int dev;
-    char buff[1024];
- 
-    dev = open("/dev/AAAA", O_RDWR);
-    printf("dev = %d\n", dev);
- 
-    int ret = read(dev, buff, 4);
-    if(ret>0)
-    {
-        printf("1. read: %d %s\n", ret, buff);
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <string.h>
+
+int LCDAddr = 0x27;
+int BLEN = 1;
+int fd;
+
+void write_word(int data){
+    int temp = data;
+    if ( BLEN == 1 )
+        temp |= 0x08;
+    else
+        temp &= 0xF7;
+    wiringPiI2CWrite(fd, temp);
+}
+
+void send_command(int comm){
+    int buf;
+    // Send bit7-4 firstly
+    buf = comm & 0xF0;
+    buf |= 0x04;                    // RS = 0, RW = 0, EN = 1
+    write_word(buf);
+    delay(2);
+    buf &= 0xFB;                    // Make EN = 0
+    write_word(buf);
+
+    // Send bit3-0 secondly
+    buf = (comm & 0x0F) << 4;
+    buf |= 0x04;                    // RS = 0, RW = 0, EN = 1
+    write_word(buf);
+    delay(2);
+    buf &= 0xFB;                    // Make EN = 0
+    write_word(buf);
+}
+
+void send_data(int data){
+    int buf;
+    // Send bit7-4 firstly
+    buf = data & 0xF0;
+    buf |= 0x05;                    // RS = 1, RW = 0, EN = 1
+    write_word(buf);
+    delay(2);
+    buf &= 0xFB;                    // Make EN = 0
+    write_word(buf);
+
+    // Send bit3-0 secondly
+    buf = (data & 0x0F) << 4;
+    buf |= 0x05;                    // RS = 1, RW = 0, EN = 1
+    write_word(buf);
+    delay(2);
+    buf &= 0xFB;                    // Make EN = 0
+    write_word(buf);
+}
+
+void init(){
+    send_command(0x33);     // Must initialize to 8-line mode at first
+    delay(5);
+    send_command(0x32);     // Then initialize to 4-line mode
+    delay(5);
+    send_command(0x28);     // 2 Lines & 5*7 dots
+    delay(5);
+    send_command(0x0C);     // Enable display without cursor
+    delay(5);
+    send_command(0x01);     // Clear Screen
+    wiringPiI2CWrite(fd, 0x08);
+}
+
+void clear(){
+    send_command(0x01);     //clear Screen
+}
+
+void write(int x, int y, char data[]){
+    int addr, i;
+    int tmp;
+    if (x < 0)  x = 0;
+    if (x > 15) x = 15;
+    if (y < 0)  y = 0;
+    if (y > 1)  y = 1;
+
+    // Move cursor
+    addr = 0x80 + 0x40 * y + x;
+    send_command(addr);
+
+    tmp = strlen(data);
+    for (i = 0; i < tmp; i++){
+        send_data(data[i]);
     }
+}
 
-    // ret = write(dev, "B", 3);
-    // printf("write: %d \n", ret);
-    
-    memset(buff, '\0', sizeof(buff)); //reset first
 
-    ret = read(dev, buff, 3);
-    if(ret>0)
-    {
-        printf("2. read: %d %s\n", ret, buff);
-    }
-
-    close(dev);
- 
-    exit(EXIT_SUCCESS);
+void main(){
+    fd = wiringPiI2CSetup(LCDAddr);
+    init();
+    write(0, 0, "Greetings!");
+    write(1, 1, "From SunFounder");
 }
