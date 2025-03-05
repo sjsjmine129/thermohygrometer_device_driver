75252fe37efb202a71449632fcd67447d1ab219c
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2025-03-05 13:29:08 +0900

    Update thermohygrometer.h

--- thermohygrometer.h
+++ thermohygrometer.h
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EROOR_WRONG_PARAMS 1
-#define EROOR_OPEN_SENSOR_DEVICE 2
-#define EROOR_OPEN_LCD_DEVICE 3
-#define EROOR_SETTING_DEVICE 4
-
+#define TH_SUCCESS 0
+#define TH_FAIL -1
+#define TH_FAIL_WRONG_PARAMS -2
+#define TH_FAIL_OPEN_SENSOR_DEVICE -3
+#define TH_FAIL_OPEN_LCD_DEVICE -4
+#define TH_FAIL_SETTING_DEVICE -5
 
 enum sensor_data_types
 {
@@ -17,8 +18,6 @@ enum sensor_data_types
 	GET_TEMPERATURE,
     GET_HUMIDITY,
 };
-
-
 
 int measure_air_condition(enum sensor_data_types sensor_data_type, int repeat, int time_gap);
 
